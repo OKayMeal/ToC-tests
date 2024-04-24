dd93da167f7c75e2b14949b4c8b2b0e264d29927
Kamil Kazimierczak <kaaaa765@gmail.com>
2024-04-24 13:43:56 +0200

    added util method for checking if lists are equal

--- BaseTest.py
+++ BaseTest.py
@@ -1,7 +1,12 @@
 import pytest
 from pages.MainMenuPage import MainMenuPage
+from collections import Counter
 
 class BaseTest():
     @pytest.fixture(autouse=True)
     def setup(self, driver):
         self.main_menu = MainMenuPage(driver, True)
+    
+    
+    def are_lists_equal(self, list1: list, list2: list):
+        return Counter(list1) == Counter(list2)
