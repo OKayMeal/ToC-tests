bca45c2ea28b64bb901a1c8c1c73810f72218276
OKayMeal <kaaaa765@gmail.com>
2024-11-16 16:19:01 +0100

    added title screen mobile test

--- test_title_screen.py
+++ test_title_screen.py
@@ -3,7 +3,7 @@ from BaseTest import BaseTest
 from pages.TitlePage import TitlePage
 
 class TestTitleScreen(BaseTest):
-    def test_TS_0001(self, driver: WebDriver, gameenv: str):
+    def test_TS_0001(self, driver: WebDriver):
         """
         Description:
         Checking If launching the app with certain screen size initializes the game in mobile mode
@@ -43,3 +43,29 @@ class TestTitleScreen(BaseTest):
         expected_text = "PRESS ENTER TO START"
         actual_text = title_screen.pressEnterButton.text
         assert  actual_text == expected_text, f"Expected text: {expected_text}. Actual text: {actual_text}"
+    
+    def test_TS_0003(self, driver: WebDriver):
+        """
+        Description:
+        Checking if launching the app in mobile mode forces the browser window to go fullscreen
+
+        Test Steps:
+        1. Initialize screen in mobile screen size
+        2. Go to Title Screen 3.
+        Tap on “Tap to Start”
+
+        Expected Result:
+        The browser window should enter fullscreen mode
+        """
+        driver.set_window_size(397, 813)
+        driver.refresh()
+
+        title_screen = TitlePage(driver)
+        window_size_before_full_screen = driver.get_window_size()
+
+        # clicking on the button should enter the full screen browser mode
+        title_screen.pressEnterButton.click()
+
+        screen_width = driver.execute_script("return screen.width")
+        screen_height = driver.execute_script("return screen.height")
+        assert window_size_before_full_screen['width'] < screen_width and window_size_before_full_screen['height'] < screen_height
