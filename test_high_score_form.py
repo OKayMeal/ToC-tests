3b76850a12efff37aafd7c6428d83b46308163f9
OKayMeal <kaaaa765@gmail.com>
2024-11-23 16:43:01 +0100

    added XSS test

--- test_high_score_form.py
+++ test_high_score_form.py
@@ -28,7 +28,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert title_page.pressEnterButton.is_displayed(), f'Not redirected to TITLE page after proceeding via CONTINUE WITHOUT SUBMITTING btn from HighScoreForm'
     
     
-    def test_HSForm_0002(self, driver: WebDriver):
+    def test_HSForm_0002(self):
         """
         Description:
         Test submitting the form with NAME  too long >30 chars
@@ -49,7 +49,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_input_value == self.high_score_form.name_input.get_attribute('value'), f"Expected value in NAME input field: {expected_input_value}. Actual value: {self.high_score_form.name_input.get_attribute('value')}"
         
     
-    def test_HSForm_0003(self, driver: WebDriver):
+    def test_HSForm_0003(self):
         """
         Description:
         Test submitting the form WITH polish profanity with polish chars as NAME
@@ -75,7 +75,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for polish profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0004(self, driver: WebDriver):
+    def test_HSForm_0004(self):
         """
         Description:
         Test submitting the form with polish profanity WITHOUT polish chars as NAME
@@ -101,7 +101,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for polish profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0005(self, driver: WebDriver):
+    def test_HSForm_0005(self):
         """
         Description:
         Test submitting the form with polish profanity mixed upper/lower cases as NAME
@@ -127,7 +127,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for polish profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0006(self, driver: WebDriver):
+    def test_HSForm_0006(self):
         """
         Description:
         Test submitting the form with polish profanity as a sub-string as NAME
@@ -153,7 +153,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for polish profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0007(self, driver: WebDriver):
+    def test_HSForm_0007(self):
         """
         Description:
         Test submitting the form with english profanity as NAME
@@ -179,7 +179,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for english profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0008(self, driver: WebDriver):
+    def test_HSForm_0008(self):
         """
         Description:
         Test submitting the form with english profanity mixed upper/lower case as NAME
@@ -205,7 +205,7 @@ class TestHighScoreForm(BaseHighScoreFormTest):
         assert expected_msg in actual_msg, f'Expected message for english profanity in NAME: {expected_msg}. Actual message: {actual_msg}'
     
     
-    def test_HSForm_0009(self, driver: WebDriver):
+    def test_HSForm_0009(self):
         """
         Description:
         Test submitting the form with english profanity as sub-string  as NAME
@@ -312,5 +312,30 @@ class TestHighScoreForm(BaseHighScoreFormTest):
                         break
                 assert not name_found, f'The record that failed to submit was found among the displayed high scores records'
                 # NOTE: THIS ASSERTION IS VALID ONLY ON DEV ENV WITH EMPTY DB, BECAUSE ON PROD IT IS PERFECTLY FINE TO SEE SEVERAL RECORDS WITH THE SAME NAME VALUE
+    
+
+    def test_HSForm_0011(self):
+        """
+        Description:
+        Test submitting the form with containing invalid characters
+        
+        Test Steps:
+        1. Go to HIGH SCORE submit form (by making player die)
+        2. In NAME field type icorrect name containing of invalid chars e.g. <, ' =
+        3. Click SUBMIT
+        
+        Expected Result:
+        3. - error should appear prompting user to type another name
+        """
+        invalid_name = "<script>alert()</script>"
+        
+        self.high_score_form.enter_name(invalid_name)
+    
+        self.high_score_form.submit_btn.click_btn()
+        
+        expected_msg = 'try another name'
+        self.high_score_form.get_form_warning()
+        actual_msg = self.high_score_form.warning.text
+        assert expected_msg in actual_msg, f'Expected message for invalid chars in NAME: {expected_msg}. Actual message: {actual_msg}'
         
         
